99e16bcee54943a1d87b51fef7ccef33ac87941c
Yuri Barbosa <127057738+YuriGit909@users.noreply.github.com>
2024-06-16 18:22:45 -0300

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -7,26 +7,40 @@
 // input button : finalizar
 // div : resultado
 
-function adicionar() {
+function adicionar(num) {
     var numero = document.getElementById('inumero')
     var num = Number(numero.value)
 
     if (numero.value.length == 0) {
-        window.alert('Preencha o campo de texto com um número de 1 a 100')
-    } else if (num <= 0 || num > 100) {
+        window.alert('Preencha o campo de texto com um número entre 1 e 100')
+    } else if (num < 1 || num > 100) {
         window.alert(`O número ${num} está fora dos limites do intervalo (1 a 100)`)
     } else {
-        let valores = document.getElementById('ivalores')
-
-        /* AJUSTAR EVENTO DEPOIS
+        var valores = document.getElementById('ivalores')
         let button = document.getElementById('addbutton')
-        let opcao = document.getElementById('opcao')
-        opcao.remove()
-        button.removeEventListener('click', this)
-        */
-
+        var opcao = document.getElementById('opcaoRemover')
+        
+        // CONDIÇÃO PARA REMOVER O OPTION INSERIDO NO HTML
+        var removido = false
+        button.addEventListener('click', () => {
+            if(!removido) {
+                opcao.remove()
+                removido = true    
+            }
+        })
+            
+        // CRIA UM NOVO OPTION DINAMICAMENTE A CADA CLIQUE DO USUÁRIO
         let option = document.createElement('option')
         option.text = `Valor ${num} adicionado`
         valores.appendChild(option)
     }
 }
+
+var armazenarNumeros = []
+
+var finalizar = document.getElementById('finalizar')
+finalizar.addEventListener('click', () => {
+    var numAdd = armazenarNumeros.push()
+    var resultado = document.getElementById('res')
+    resultado.innerHTML = `<p>${numAdd}</p>`
+})
