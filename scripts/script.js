c1aab2f7c2d4cecdd107be4c9b7cd57160351b34
Yuri Barbosa <127057738+YuriGit909@users.noreply.github.com>
2024-06-16 17:57:21 -0300

    Altered

--- scripts/script.js
+++ scripts/script.js
@@ -12,10 +12,21 @@ function adicionar() {
     var num = Number(numero.value)
 
     if (numero.value.length == 0) {
-        window.alert('Preencha o campo de texto com um número de 0 a 100')
-    } else if (num > 100) {
-        window.alert(`O número ${num} está acima do valor máximo permitido!`)
+        window.alert('Preencha o campo de texto com um número de 1 a 100')
+    } else if (num <= 0 || num > 100) {
+        window.alert(`O número ${num} está fora dos limites do intervalo (1 a 100)`)
     } else {
-        
+        let valores = document.getElementById('ivalores')
+
+        /* AJUSTAR EVENTO DEPOIS
+        let button = document.getElementById('addbutton')
+        let opcao = document.getElementById('opcao')
+        opcao.remove()
+        button.removeEventListener('click', this)
+        */
+
+        let option = document.createElement('option')
+        option.text = `Valor ${num} adicionado`
+        valores.appendChild(option)
     }
 }
